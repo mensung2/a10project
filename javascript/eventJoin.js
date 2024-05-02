a17e5eddc326b9a45ae02136bda770921a52c92c
Joseph Park <rkfnahs12e@gmail.com>
2024-05-02 14:34:56 +0900

    add : eventGuide.js, eventJoin.js 생성 - eventJoin.js에 해당 페이지 작업 완료

---
+++ javascript/eventJoin.js
@@ -0,0 +1,74 @@
+// html에 div태그를 만들어주는 함수
+const createDiv = (id, text) => {
+  const div = document.createElement("div");
+  div.id = id;
+  div.innerText = text;
+  return div;
+};
+const makeDivObj = () => {
+  const containerDiv = createDiv("event-join-container", "");
+  const headerDiv = createDiv("header", "");
+  const mainDiv = createDiv("main", "");
+  const ticketDiv = createDiv("ticket", "");
+  const footerDiv = createDiv("footer", "");
+  return { containerDiv, headerDiv, mainDiv, ticketDiv, footerDiv };
+};
+//아스키 코드 생성기 A 65
+const askiiCodeGenerator = (askiiNum) => {
+  const askiiString = String.fromCharCode(askiiNum);
+
+  return askiiString;
+};
+
+const checkSeatGrade = (seatCode) => {
+  if (seatCode === "A5" || seatCode === "A6") return "VIP";
+  console.log("seatCode[-1]", seatCode[1]);
+  if (seatCode[1] === "0") return "seat-en";
+  if (seatCode.includes("A")) return "R";
+  if (
+    seatCode.includes("B") ||
+    seatCode.includes("C") ||
+    seatCode.includes("D")
+  )
+    return "S";
+  return "A";
+};
+const makeSeat = () => {
+  const seat = createDiv("seat", "");
+  const seatRow = 11;
+  const seatCol = 10;
+  let seatGrade = "";
+  for (let i = 0; i < seatCol; i++) {
+    const askiiChar = askiiCodeGenerator(65 + i);
+    let seatColumnHTML = "";
+    for (let j = 0; j < seatRow; j++) {
+      if (j === 0) {
+        seatGrade = checkSeatGrade(askiiChar + j);
+        seatColumnHTML += `<div id="${askiiChar}${j}" class = "${seatGrade} , seat-element">${askiiChar}</div>`;
+      }
+      if (j !== 0) {
+        seatGrade = checkSeatGrade(askiiChar + j);
+        seatColumnHTML += `<div id="${askiiChar}${j}" class = "${seatGrade} , seat-element">${j}</div>`;
+      }
+    }
+    seat.innerHTML += `<div id="${askiiChar}-container" class = "seat-container">${seatColumnHTML}</div>`;
+  }
+  console.log("seat.innerHTMl", seat.innerHTML);
+  return seat.innerHTML;
+};
+
+const renderPage = () => {
+  const divObj = makeDivObj();
+  console.log(divObj);
+  divObj.containerDiv.innerHTML = `
+  ${(divObj.headerDiv.innerHTML = `<h1>Nav</h1>`)}
+  ${(divObj.mainDiv.innerHTML = `
+  <div id = "main-screen">무대</div>
+  <div id = "main-seat">${makeSeat()}</div>
+  `)}
+  ${(divObj.ticketDiv.innerHTML = `<h1>ticket Image</h1>`)}
+  ${(divObj.footerDiv.innerHTML = `<h1>Footer</h1>`)}
+  `;
+  document.body.appendChild(divObj.containerDiv);
+};
+renderPage();
