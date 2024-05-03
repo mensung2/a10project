ccc9f43888aa2772139755ffe96a34ec51a82fee
김태건 <taegun@gimtaegeon-ui-MacBookPro.local>
2024-05-03 19:28:36 +0900

    필요없는코드삭제

--- javascript/search.js
+++ javascript/search.js
@@ -45,37 +45,3 @@ document.querySelector('.search-btn').addEventListener('click', async function (
     event.preventDefault();
     updateScreens();
 });
-
-
-// const searchBox = document.querySelector(".search-txt");
-// const searchButton = document.querySelector(".search-btn");
-// const cardList = document.querySelector(".card-list");
-// const movieCards = document.querySelectorAll('.movie-card');
-
-// searchButton.addEventListener('click', async function (event) {
-//     event.preventDefault();
-//     const searchText = searchBox.value.trim().toLowerCase();
-//     const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText}&region=KR&language=ko-KR&api_key=01cc7b9c736a3d881c0752e5a7255a9b`)
-//         .then(response => response.json())
-//         .catch(err => console.error(err));
-//     console.log(res)
-//     movieCards.innerHtml = '';
-//     res.results.forEach(data => {
-//         console.log(data);
-//         const div = document.createElement("div");
-//         div.setAttribute("class", "movie-card");
-//         div.setAttribute("id", "data.id")
-
-//         const img = document.createElement("img");
-//         img.src = `https://image.tmdb.org/t/p/w300/${data.poster_path}`;
-
-//         const title = document.createElement("h3");
-//         title.innerText = data.title;
-
-//         const overview = document.createElement("p");
-//         overview.innerText = data.overview;
-
-//         div.append(img, title, overview,);
-//         cardList.append(div);
-//     });
-// });
