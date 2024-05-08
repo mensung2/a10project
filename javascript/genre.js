c4a6d82917b2c35a8a64db2bb081b5b45a76f2ed
sseulnim <hanbat107@naver.com>
2024-05-08 11:02:23 +0900

    save

--- javascript/genre.js
+++ javascript/genre.js
@@ -63,12 +63,41 @@ function displayMovies(movies, containerId) {
   });
 }
 
+const updateMovieList = (movies) => {
+  const container = document.getElementById("movie-cards");
+  container.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const title = movie.title;
+    const voteAverage = movie.vote_average;
+    const overview = movie.overview;
+    const posterPath = movie.poster_path;
+    const id = movie.id;
+
+    const card = document.createElement("div");
+    card.classList.add("movie_card");
+    card.id = id;
+
+    // 이미지 클릭하면 해당 영화의 id값을 가진 페이지로 이동하도록 <a> 태그 추가
+    card.innerHTML = `
+     <div>  
+        <a id = 'clickevent 'href = 'detailReview.html?id=${id}'> 
+        <img src="https://image.tmdb.org/t/p/w200${posterPath}" id="poster" alt="${title}" /></a>
+        <h3>${title}</h3>
+        <p id="score">★ ${voteAverage}</p>
+        <p id="content">${overview}</p>               
+     </div>
+    `;
+    container.appendChild(card);
+  });
+};
+
 // navi바 표시
 const getNav = () => {
-  fetch("../nav.html")
+  fetch("./nav.html")
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("header-nav").innerHTML = data;
+      document.getElementById("nav").innerHTML = data;
     })
     .catch((error) => console.error("Error:", error));
 };
