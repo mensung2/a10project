7d1e45f85e4abf07c37905b8064d7baffd2d8edc
Joseph Park <rkfnahs12e@gmail.com>
2024-05-08 21:20:22 +0900

    update : nav.html에서 search기능은 가능함

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,6 +1,13 @@
-const backgSpaceButton = document.getElementById("section-container-button");
+const searchButton = document.getElementsByClassName("search-btn");
+const searchInput = document.getElementsByClassName("search-txt");
+let searchCount = 0;
+let movieName = "";
+searchInput[0].addEventListener("change", (e) => {
+  movieName = e.target.value;
+  localStorage.setItem("movieName", JSON.stringify(e.target.value));
+});
 
-backgSpaceButton.addEventListener("click", (e) => {
-  console.log("e", e);
-  history.go(1);
+searchButton[0].addEventListener("click", async (e) => {
+  e.preventDefault();
+  window.location.href = "../search.html";
 });
