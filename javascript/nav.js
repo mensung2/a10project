b8743be79823b48642db974831319dc829cb5b43
Joseph Park <rkfnahs12e@gmail.com>
2024-05-09 10:48:55 +0900

    search.html의 nav 수정

--- javascript/nav.js
+++ javascript/nav.js
@@ -2,11 +2,13 @@ const searchButton = document.getElementsByClassName("search-btn");
 const searchInput = document.getElementsByClassName("search-txt");
 let searchCount = 0;
 let movieName = "";
-const search = () =>
+const search = () => {
   searchInput[0].addEventListener("blur", (e) => {
+    e.preventDefault();
     movieName = e.target.value;
     localStorage.setItem("movieName", JSON.stringify(e.target.value));
   });
+};
 
 const button = () =>
   searchButton[0].addEventListener("click", async (e) => {
