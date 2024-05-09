5fcfbd9080e32d63cc4a764a7ecba4ddeff54da7
kwakjaehun <jh5414092@icloud.com>
2024-05-09 11:33:45 +0900

    save

--- javascript/nav.js
+++ javascript/nav.js
@@ -2,11 +2,19 @@ const searchButton = document.getElementsByClassName("search-btn");
 const searchInput = document.getElementsByClassName("search-txt");
 let searchCount = 0;
 let movieName = "";
-const search = () =>
+const search = () => {
   searchInput[0].addEventListener("blur", (e) => {
     movieName = e.target.value;
     localStorage.setItem("movieName", JSON.stringify(e.target.value));
   });
+  searchInput[0].parentElement.addEventListener("submit", e => {
+    e.preventDefault();
+    movieName = e.target[0].value;
+    localStorage.setItem("movieName", JSON.stringify(e.target[0].value));
+    window.location.href = "../search.html";
+  })
+}
+  
 
 const button = () =>
   searchButton[0].addEventListener("click", async (e) => {
